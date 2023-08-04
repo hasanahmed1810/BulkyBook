8ebfd058b6c79fca3f6351a853f97aa20ffe445e
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2023-08-04 22:39:21 +0300

    Add project files.

---
+++ ecommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
