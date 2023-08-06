7796dbc29b2d96a832bd25d96c1f00ece0f95bf8
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2023-08-06 17:25:02 +0300

    added products

---
+++ ecommerce/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,152 @@
+﻿using ecommerce.DataAccess;
+using ecommerce.Models;
+using ecommerce.Repository;
+using ecommerce.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ecommerce.Areas.Admin.Controllers
+{
+
+    public class ProductController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+
+            
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem 
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }
+                );
+
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+               u => new SelectListItem
+               {
+                   Text = u.Name,
+                   Value = u.Id.ToString(),
+               }
+               );
+
+            ViewBag.CategoryList = CategoryList;
+            ViewBag.CoverTypeList = CoverTypeList;
+
+            if (id == null || id == 0)
+            {
+
+              
+                return View(product);
+            } else
+            {
+                
+                product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+                return View(product);
+            }
+
+        }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public IActionResult Upsert(Product obj, IFormFile file)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extention = Path.GetExtension(file.FileName);
+
+                    if (obj.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extention), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    obj.ImageUrl = @"\images\products\" + fileName + extention;
+                }
+
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj);
+                    TempData["success"] = "Product created successfully";
+
+
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj);
+                    TempData["success"] = "Product updated successfully";
+
+                }
+
+                _unitOfWork.Save();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return Json(new {data =  productList});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(item => item.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+        #endregion
+    }
+
+}
---
+++ ecommerce/Repository/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using ecommerce.DataAccess;
+using ecommerce.Models;
+using ecommerce.Repository.IRepository;
+
+namespace ecommerce.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(item => item.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
