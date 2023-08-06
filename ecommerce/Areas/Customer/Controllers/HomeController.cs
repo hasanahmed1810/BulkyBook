b726d0da7a7fd46467f8dde2591b613f2a7d7e81
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2023-08-06 19:37:23 +0300

    added home page

--- ecommerce/Areas/Customer/Controllers/HomeController.cs
+++ ecommerce/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using ecommerce.Models;
+using ecommerce.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,18 +8,34 @@ namespace ecommerce.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
+
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+
+            return View(productList);
         }
 
-        public IActionResult Privacy()
+		public IActionResult Details(int id)
+		{
+			ShoppingCart cartObj = new()
+			{
+				Count = 1,
+				Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+			};
+
+			return View(cartObj);
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
